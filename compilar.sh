e0c320289eeab9230dc0cfdc453d75c6ce4ee304
Elchino3000 <41701473+Elchino3000@users.noreply.github.com>
2019-06-12 10:32:20 -0500

    Update compilar.sh

--- compilar.sh
+++ compilar.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-g++ `pkg-config --cflags opencv` cromaticidad.cpp -o croma_art `pkg-config --libs opencv` -std=c++11
+g++ `pkg-config --cflags opencv` histograms.cpp -o croma_art `pkg-config --libs opencv` -std=c++11
 
